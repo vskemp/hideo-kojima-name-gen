e76f6bf406e79d787d0484951c0e955281155c3c
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-12-19 11:45:11 -0500

    most submit buttons functional

---
+++ index.js
@@ -0,0 +1,104 @@
+function sec2Num1() {
+    let namein = document.getElementById("fullName").value;
+    display_namein.innerHTML = namein;
+}
+
+function sec7Num2a() {
+    let jobin = document.getElementById("oneOccupation").value;
+    display_jobin.innerHTML = jobin;
+}
+
+function sec8Num1() {
+    let petin = document.getElementById("petType").value;
+    display_petin.innerHTML = petin;
+}
+
+function sec9Num2Three() {
+    let memin =document.getElementById("twoMemory").value;
+    display_memin.innerHTML = memin;
+}
+
+function sec11Num1() {
+    let stabin = document.getElementById("stabbedBy").value;
+    display_stabin.innerHTML = stabin;
+}
+
+function sec10Num2Four() {
+    let goodin = document.getElementById("goodAt").value;
+    display_goodin.innerHTML = goodin;
+}
+
+function sec7Num2Three() {
+    let carrotin = document.getElementById("carrotNum").value;
+    display_carrotin.innerHTML = carrotin;
+}
+
+function sec9Num2One() {
+    let intantin = document.getElementById("intanFear").value;
+    display_intantin.innerHTML = intantin;
+}
+
+function sec9Num2Two() {
+    let tanin = document.getElementById("tanFear").value;
+    display_tanin.innerHTML = tanin;
+}
+
+function sec12Num5() {
+    let quizin = document.getElementById("beforeQuiz").value;
+    display_quizin.innerHTML = quizin;
+}
+
+function sec4Num2Eight() {
+    let bodyin = document.getElementById("bodyCondition").value;
+    display_bodyin.innerHTML = bodyin;
+}
+
+function sec8Num2One() {
+    let matterin = document.getElementById("favMatter").value;
+    display_matterin.innerHTML = matterin;
+}
+
+function sec10Num2Six() {
+    let soundin = document.getElementById("nameSound").value;
+    display_soundin.innerHTML = soundin;
+}
+
+function sec8Num2Four() {
+    let signin = document.getElementById("zodiacSign").value;
+    display_signin.innerHTML = signin;
+}
+
+function sec7Num2One() {
+    let personin = document.getElementById("onePersonality").value;
+    display_personin.innerHTML = personin;
+}
+
+function sec7Num2Four() {
+    let kurtin = document.getElementById("kurtRussell").value;
+    display_kurtin.innerHTML = kurtin;
+}
+
+function sec10Num2One() {
+    let kubrikin = document.getElementById("kubrikFilm").value;
+    display_kubrikin.innerHTML = kubrikin;
+}
+
+function sec10Num2Two() {
+    let joyin = document.getElementById("joyDivision").value;
+    display_joyin.innerHTML = joyin;
+}
+
+function sec10Num2Three() {
+    let termin = document.getElementById("nprTerm").value;
+    display_termin.innerHTML = termin;
+}
+
+function sec9Num2Four() {
+    let warin = document.getElementById("warBad").value;
+    display_warin.innerHTML = warin;
+}
+
+function sec10Num1() {
+    let madin = document.getElementById("madsOne").value;
+    display_madin.innerHTML = madin;
+}
